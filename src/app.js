e86091e66cca122f9570edbb7e777724b5772bb1
Sachin Parmar <sachinparmar4160@gmail.com>
2024-11-19 17:23:10 +0530

    the app file for routes

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const userRoutes = require('./routers/user.route.js');
+
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({
+    path: './.env'
+});
+const app = express();
+
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/user', userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ error: err.message });
+});
+
+module.exports = app;
